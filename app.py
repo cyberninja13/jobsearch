17f40ecfe2fa671e0be78f793a152ba7f3472670
cyberninja13 <120660656+cyberninja13@users.noreply.github.com>
2023-11-27 04:23:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 import streamlit as st
 from jobspy.jobs import Job  # adjust based on your actual module structure
-from jobspy.scrapers import YourScraper  # adjust based on your actual module structure
 
-st.title("JobSpy Streamlit App")
+def main():
+    st.title("JobSpy Streamlit App")
 
-# Input for Job Search
-job_search = st.text_input("Enter Job Title", "Data Scientist")
+    # Input for Job Search
+    job_search = st.text_input("Enter Job Title", "Data Scientist")
 
-# Input for Location
-location = st.text_input("Enter Location", "New York")
+    # Input for Location
+    location = st.text_input("Enter Location", "New York")
 
-# Create Job instance with user inputs
-job = Job(job_search, location)
+    # Create Job instance with user inputs
+    job = Job(job_search, location)
 
-# Create Scraper instance if needed
-scraper = YourScraper()  # adjust based on your actual module structure
+    # Display Job information
+    st.write(f"Job Title: {job.title}")
+    st.write(f"Location: {job.location}")
 
-# Use Job and Scraper as needed
-job_results = job.get_results()
-scraper_data = scraper.scrape()  # adjust based on your actual module structure
+    # Continue with the rest of your app logic...
 
-# Display Results
-st.write("Job Search Results:")
-st.dataframe(job_results)
+if __name__ == "__main__":
+    main()
