9f5f404f9b1baa57e10a82f7bf0e30ff1c7c7abd
cyberninja13 <120660656+cyberninja13@users.noreply.github.com>
2023-11-27 04:40:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from jobspy.jobs.job_module import JobType, Location  # adjust based on your actual module structure
+from jobspy.jobs import JobType, Location  # adjust based on your actual module structure
+from jobspy.scrapers import glassdoor, indeed, linkedin, ziprecruiter  # adjust based on your actual module structure
 
 def main():
     st.title("JobSpy Streamlit App")
@@ -13,6 +14,12 @@ def main():
     # Use JobType and Location as needed
     selected_job_type = st.selectbox("Select Job Type", list(JobType))
     selected_location = st.selectbox("Select Location", list(Location))
+
+    # Use scrapers as needed
+    glassdoor_data = glassdoor.scrape()  # adjust based on your actual module structure
+    indeed_data = indeed.scrape()  # adjust based on your actual module structure
+    linkedin_data = linkedin.scrape()  # adjust based on your actual module structure
+    ziprecruiter_data = ziprecruiter.scrape()  # adjust based on your actual module structure
 
     # Continue with the rest of your app logic...
 
