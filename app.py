1789dac382dcf80f8ec5966773ab6009f941b65b
cyberninja13 <120660656+cyberninja13@users.noreply.github.com>
2023-11-27 05:54:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import asyncio
-from rich.progress import track
-from rich.console import Console
 from rich.table import Table
+from rich.console import Console
 
 # Instantiate global variables
 df = pd.DataFrame(columns=["Title", "Location", "Company", "Link", "Description"])
@@ -14,78 +11,38 @@ console = Console()
 table = Table(show_header=True, header_style="bold")
 
 # Function to scrape job description
-async def scrapeJobDescription(url):
-    global df
-    driver = DriverOptions()
-    driver.get(url)
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
+def scrapeJobDescription(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
     try:
-        jobDescription = soup.find(
-            "div", class_="show-more-less-html__markup"
-        ).text.strip()
+        jobDescription = soup.find("div", class_="show-more-less-html__markup").text.strip()
         return jobDescription
-    except:
+    except AttributeError:
         return ""
 
-# Function to set up WebDriver options
-def DriverOptions():
-    options = Options()
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--headless=new")
-    driver = webdriver.Chrome(options=options)
-    return driver
-
-# Asynchronous function to scrape LinkedIn jobs
-async def scrapeLinkedin(inputJobTitle, inputJobLocation):
-    global df
-    driver = DriverOptions()
+# Function to scrape LinkedIn jobs
+def scrapeLinkedin(inputJobTitle, inputJobLocation):
     counter = 0
     pageCounter = 1
-    
+
     while True:
         try:
-            driver.get(
-                f"https://www.linkedin.com/jobs/search/?&keywords={inputJobTitle}&location={inputJobLocation}&refresh=true&start={counter}"
-            )
+            url = f"https://www.linkedin.com/jobs/search/?&keywords={inputJobTitle}&location={inputJobLocation}&refresh=true&start={counter}"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
 
-            html = driver.page_source
-            soup = BeautifulSoup(html, "html.parser")
+            jobs = soup.select("li.jobs-search__result-item")
 
-            ulElement = soup.find("ul", class_="jobs-search__results-list")
-            liElements = ulElement.find_all("li")
+            for job in jobs:
+                jobTitle = job.select_one("h3.base-search-card__title").text.strip()
+                jobLocation = job.select_one("span.job-search-card__location").text.strip()
+                jobCompany = job.select_one("h4.base-search-card__subtitle").text.strip()
+                jobLink = job.select_one("a")["href"]
 
-            for item in track(
-                liElements, description=f"Linkedin - Page: {pageCounter}"
-            ):
-                jobTitle = item.find(
-                    "h3", class_="base-search-card__title"
-                ).text.strip()
-                jobLocation = item.find(
-                    "span", class_="job-search-card__location"
-                ).text.strip()
-                jobCompany = item.find(
-                    "h4", class_="base-search-card__subtitle"
-                ).text.strip()
-                jobLink = item.find_all("a")[0]["href"]
-
-                jobDescription = await scrapeJobDescription(jobLink)
+                jobDescription = scrapeJobDescription(jobLink)
 
                 if jobTitle and jobLocation and jobCompany and jobLink:
-                    df = pd.concat(
-                        [
-                            df,
-                            pd.DataFrame(
-                                {
-                                    "Title": [jobTitle],
-                                    "Location": [jobLocation],
-                                    "Company": [jobCompany],
-                                    "Link": [jobLink],
-                                    "Description": [jobDescription],
-                                }
-                            ),
-                        ]
-                    )
+                    df.loc[len(df.index)] = [jobTitle, jobLocation, jobCompany, jobLink, jobDescription]
 
             console.print("Scrape Next Page? (y/n) :", style="bold yellow", end=" ")
             continueInput = input()
@@ -96,13 +53,12 @@ async def scrapeLinkedin(inputJobTitle, inputJobLocation):
             counter += 25
             pageCounter += 1
 
-        except:
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
             break
 
-    driver.quit()
-
-# Asynchronous main function
-async def main():
+# Streamlit UI
+def main():
     st.title("LinkedIn Job Scraper")
 
     # Get user input using Streamlit
@@ -110,7 +66,7 @@ async def main():
     inputJobLocation = st.text_input("Enter Job Location:")
 
     if st.button("Scrape LinkedIn"):
-        await scrapeLinkedin(inputJobTitle, inputJobLocation)
+        scrapeLinkedin(inputJobTitle, inputJobLocation)
 
         # Create table using Streamlit
         st.write("Scraped Jobs:")
@@ -122,5 +78,4 @@ async def main():
             st.success("Results saved successfully!")
 
 if __name__ == "__main__":
-    # Run the Streamlit app
-    asyncio.run(main())
+    main()
