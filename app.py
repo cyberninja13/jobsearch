035b71079290676699576e5137247733a75e7454
cyberninja13 <120660656+cyberninja13@users.noreply.github.com>
2023-11-28 04:32:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ Created on Mon Mar 20 20:27:50 2023
 
 import time
 import csv
-from bs4 import BeautifulSoup
 import streamlit as st
 from selenium import webdriver
-import chromedriver_autoinstaller
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
 # Function to scrape Indeed jobs
 def scrape_indeed_jobs(query, location, num_pages):
@@ -17,18 +17,12 @@ def scrape_indeed_jobs(query, location, num_pages):
     base_url = 'https://in.indeed.com'
     
     job_data = []
-    
-    chromedriver_autoinstaller.install()  # Automatically installs the latest version of ChromeDriver
-    
-    driver = webdriver.Chrome()
-    
+
     for start in start_list:
         url = base_url + f'/jobs?q={query}&l={location}&start={start}'
-        driver.execute_script(f"window.open('{url}', 'tab{start}');")
+        st.experimental_singleton(driver, webdriver.Chrome, ChromeDriverManager().install())
+        driver.get(url)
         time.sleep(1)
-    
-    for start in start_list:
-        driver.switch_to.window(f'tab{start}')
     
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         items = soup.find_all('td', {'class': 'resultContent'})
@@ -49,8 +43,6 @@ def scrape_indeed_jobs(query, location, num_pages):
     
             job_data.append([job_title, company, location, job_link, salary])
     
-    driver.quit()
-    
     return job_data
 
 # Function to display Streamlit app
@@ -65,22 +57,10 @@ def main():
         job_data = scrape_indeed_jobs(query, location, num_pages)
         
         st.write("Scraped Job Data:")
-        st.table(job_data)  # Display the data in a table
-        
-        # You can also show the data in a more customized way, for example:
-        # for job in job_data:
-        #     st.write(f"Job Title: {job[0]}")
-        #     st.write(f"Company: {job[1]}")
-        #     st.write(f"Location: {job[2]}")
-        #     st.write(f"Job Link: {job[3]}")
-        #     st.write(f"Salary: {job[4]}")
-        #     st.write("---")
-        
-        # You can still save the data to a CSV file if needed, but it won't be necessary for display
-        # with open(f'{query}_{location}_job_results.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        #     writer = csv.writer(csvfile)
-        #     writer.writerow(['Job Title', 'Company', 'Location', 'Job Link', 'Salary'])
-        #     writer.writerows(job_data)
+        st.table(job_data)
+
+# Create a global driver instance
+driver = webdriver.Chrome()
 
 # Run the Streamlit app
 if __name__ == "__main__":
