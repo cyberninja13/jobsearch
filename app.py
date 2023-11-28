88cf37ad352108c82be2ad794eabf604ea345840
cyberninja13 <120660656+cyberninja13@users.noreply.github.com>
2023-11-28 02:43:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import asyncio
 import time
 import logging
 from playwright.sync_api import sync_playwright
-import pandas as pd  # Make sure to import pandas
+import pandas as pd
 
 # Configure logging to display debug information
 logging.basicConfig(level=logging.DEBUG)
@@ -12,30 +12,13 @@ logger = logging.getLogger(__name__)
 if 'data' not in st.session_state:
     st.session_state.data = []
 
-def display_data() -> None:
-    edited_df = st.data_editor(pd.DataFrame(st.session_state.data),
-        column_order=("post_date", 
-                    "title", "location", 
-                    "employer", "salary",
-                    "job_description",
-                    "job_highlights",
-                    "url"),
-        column_config={
-                    "post_date": "Date posted",
-                    "title": "Title",
-                    "location": "Location",
-                    "employer": "Employer",
-                    "salary": "Salary",
-                    "job_description": st.column_config.TextColumn("Description", width="large"),
-                    "job_highlights": st.column_config.TextColumn("Highlights", width="large"),
-                    "url": st.column_config.LinkColumn("URL", width="medium")
-        }
-    )
+def display_data():
+    edited_df = st.data_editor(pd.DataFrame(st.session_state.data))
 
 def run(playwright, max_scroll, query, browser):
     # Your run function implementation
 
-def main() -> None:
+def main():
     st.set_page_config(layout="wide")
     st.title("Search Jobs")
     positions = st.text_input("Enter comma separated position titles")
@@ -51,16 +34,11 @@ def main() -> None:
             start_time = time.perf_counter()
 
             with sync_playwright() as p:
-                # Use Playwright's environment variable to specify the browser executable
                 p._env["WEBKIT_EXECUTABLE_PATH"] = "/usr/bin/webkitgtk"
                 browser = p.webkit.launch(headless=True)
-                run(
-                    p,
-                    max_scroll=3,
-                    query=f"{str(positions)} in {str(location)}",
-                    browser=browser
-                )
+                run(p, max_scroll=3, query=f"{str(positions)} in {str(location)}", browser=browser)
                 display_data()
+                
                 minutes = (time.perf_counter() - start_time) / 60
                 logger.debug(f"Time elapsed: {round(minutes, 1)} minutes")
 
