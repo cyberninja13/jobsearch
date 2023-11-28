c00a771a87c775a0f78af4d2becddcea36ababe3
cyberninja13 <120660656+cyberninja13@users.noreply.github.com>
2023-11-28 02:58:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
+import asyncio
 import time
 import logging
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 import pandas as pd
 from extract import run
 
-# Configure logging to display debug information
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 if 'data' not in st.session_state:
     st.session_state.data = []
 
-def display_data():
+def display_data() -> None:
     edited_df = st.data_editor(pd.DataFrame(st.session_state.data),
         column_order=("post_date", 
                     "title", "location", 
@@ -32,14 +32,15 @@ def display_data():
         }
     )
 
-def main():
+async def main() -> None:
+    """The `main` function uses Playwright to run a search query for a given term in the given location."""
     st.set_page_config(layout="wide")
     st.title("Search Jobs")
     positions = st.text_input("Enter comma separated position titles")
     location = st.text_input("Enter location")
 
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col3:
+    col1, col2, col3 , col4, col5 = st.columns(5)
+    with col3 :
         search = st.button('Search')
 
     if search:
@@ -47,13 +48,15 @@ def main():
             st.session_state.data = []
             start_time = time.perf_counter()
 
-            with sync_playwright() as p:
-                browser = p.chromium.launch(headless=True)
-                run(p, max_scroll=3, query=f"{str(positions)} in {str(location)}", browser=browser)
-                display_data()
-                
-                minutes = (time.perf_counter() - start_time) / 60
-                logger.debug(f"Time elapsed: {round(minutes, 1)} minutes")
+            async with async_playwright() as playwright:
+                await run(
+                    playwright,
+                    max_scroll=3,
+                    query=f"{str(positions)} in {str(location)}",
+                )
+            display_data()
+            minutes = (time.perf_counter() - start_time) / 60
+            logger.debug(f"Time elapsed: {round(minutes, 1)} minutes")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
