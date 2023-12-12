f0bd88de0313de78767d200385812e968ef09989
cyberninja13 <120660656+cyberninja13@users.noreply.github.com>
2023-12-12 02:14:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def make_clickable(link):
     return f'<a href="{link}" target="_blank">{link}</a>'
 
 def main():
-    st.title("HiresMe.ai Job Search - Linkedin, Indeed, Monster, Glassdoor ")
+    st.title("Job Search App")
 
     # Load data
     df = load_data()
@@ -25,6 +25,9 @@ def main():
     if df is not None:
         # Job Title search input
         job_title_search = st.text_input("Enter Job Title to search:", "")
+
+        # Number of columns per page
+        columns_per_page = 100
 
         # Search button
         if st.button("Search"):
@@ -34,29 +37,20 @@ def main():
             # Apply clickable links to the "Link" column
             filtered_df["Link"] = filtered_df["Link"].apply(make_clickable)
 
-            # Display the filtered data with st.markdown
-            st.markdown(
-                """
-                <style>
-                    table {
-                        color: black;
-                        user-select: none; /* Disable text selection */
-                        -moz-user-select: none;
-                        -webkit-user-select: none;
-                        -ms-user-select: none;
-                    }
-                    th, td {
-                        border: 1px solid black;
-                        padding: 8px;
-                        text-align: left;
-                    }
-                </style>
-                """, 
-                unsafe_allow_html=True
-            )
+            # Display the filtered data with st.dataframe and pagination
+            st.dataframe(filtered_df[['A', 'B', 'C', 'Link']].head(columns_per_page), unsafe_allow_html=True)
 
-            # Explicitly include all columns
-            st.markdown(filtered_df.to_html(escape=False, index=False), unsafe_allow_html=True)
+            # Allow pagination with checkboxes
+            show_additional_pages = st.checkbox("Show additional pages")
+            if show_additional_pages:
+                remaining_columns = filtered_df.shape[1] - columns_per_page
+                num_pages = remaining_columns // columns_per_page + 1
+
+                for page in range(1, num_pages + 1):
+                    st.subheader(f"Page {page}")
+                    start_idx = page * columns_per_page
+                    end_idx = (page + 1) * columns_per_page
+                    st.dataframe(filtered_df.iloc[:, start_idx:end_idx], unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
