e3edd742e3f73cad4cbe7d78d087f621ffed08de
cyberninja13 <120660656+cyberninja13@users.noreply.github.com>
2023-12-11 23:41:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def main():
             filtered_df = df[df["Job Title"].str.contains(job_title_search, case=False)]
 
             # Display the filtered data in a wider table
-            st.table(filtered_df.style.format({'link': make_clickable}))
+            st.table(filtered_df.style.format({'Link': make_clickable}))
 
 if __name__ == "__main__":
     main()
